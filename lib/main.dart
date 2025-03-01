0eb754b705c04ea27bf885e223ac42f407f45da6
tushar-ventura <48640828+trick4u@users.noreply.github.com>
2025-03-02 01:09:19 +0530

    app_icon_gk

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart'; // Fixed import
-
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'firebase_options.dart';
 import 'infrasturcture/dtos/todo_task_dto.dart';
 import 'presentation/pages/task_list_page.dart'; // Import DTO
 
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -17,6 +19,9 @@ void main() async {
   await Hive.initFlutter(); 
   Hive.registerAdapter(TodoTaskDtoAdapter());
   await Hive.openBox<TodoTaskDto>('tasks'); 
+  const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+  const InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   runApp(const ProviderScope(child: MyApp()));
 }
 
